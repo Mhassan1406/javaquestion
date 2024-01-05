a7b289f4dab6107a703e052f41bd72f7d5889aa4
unknown <mhhassan.arif2000@gmail.com>
2024-01-05 14:07:16 -0800

    work complete

---
+++ app.js
@@ -0,0 +1,233 @@
+chapter 1
+// // // // // // // // // // //  alert("Muhummad");
+// // // // // // // // // // //  alert("Hasan");
+// // // // // // // // // // //  alert("mhhassan.arif2000@gmail.com");
+// // // // // // // // // // //  alert("03193919005");
+// // // // // // // // // // //  alert("hasan12345");
+
+
+chapter 2
+// // // // // // // // // // alert("your'e learning javascript!");
+// // // // // // // // // alert("my name is hasan");
+
+chapter3
+// // // // // // // // // var firstName;
+// // // // // // // // // var  secondName;
+// // // // // // // // // secondName ="hasan"
+
+chapter4
+// // // // // // // // // var teamName ="arcane fc"
+// // // // // // // // // alert(teamName);
+
+
+// // // // // // // // var bestMan ="Charlie"
+
+chapter 5
+// // // // // // // // var caseQty;
+
+// // // // // // // // var caseQty=144;
+
+// // // // // // // // var num =9;
+
+// // // // // // // // var num1;
+// // // // // // // // var num1 =1+1;
+chapter7
+// // // // // // // // var merhTotal=100;
+// // // // // // // // var shippingCharge=10;
+// // // // // // // // var orderTotal=merhTotal+shippingCharge;
+// // // // // // // // alert(orderTotal);
+chapter8
+// // // // // // // // var num1=10;
+// // // // // // // // num1 =num1+10;
+// // // // // // // // alert(num1);
+
+chapter  9
+// // // // // // // // var productCost= 3.45;
+
+// // // // // // // // var NameOfBand;
+
+// // // // // // // // var num1 ="92";
+
+// // // // // // // var hasan;
+chapter 10
+// // // // // // var 10person illegal
+// // // // // // var person10 legal
+// // // // // // var person-1 illegal
+// // // // // // var person_1 legal
+
+// // // // // // modulus  %
+
+chapter 11
+// // // // // // var num=20%6;
+// // // // // // alert(num);
+
+
+// // // // // // var LargeNum = 1000 *2000;
+// // // // // // alert (LargeNum)
+
+chapter 12
+// // // // // // var num1 =30;
+// // // // // // var num2=15
+// // // // // // var total =num1-num2;
+// // // // // // alert(total)
+
+chapter 13
+// // // // // var num4 =20%2;
+// // // // // alert(num4);
+
+// // // // // var  num = 10*20;
+// // // // // alert(num);
+
+
+chapter 14
+// // // //  var x=x+1;
+// // // // +x ,x+;
+
+
+// // // // var x =100;
+// // // // var b =x--;
+// // // // alert(x);
+
+
+// // // // var x=50;
+// // // // var y=x++;
+// // // // alert(y)
+
+
+// // // // var y=50;
+// // // // var z=--y;
+// // // // alert(z);
+
+// // // // var num1 =20;
+// // // // var newNum =num1--;
+// // // // alert(newNum);
+
+
+// // // // var a=50;
+// // // // var b=a++;
+// // // // alert(b);
+
+
+// // // // var x =20;
+// // // // var y=++x;
+// // // // alert(y);
+
+chapter 15
+// // // alert(14);
+// // // alert(24);
+// // // alert(24);
+// // // alert(18);
+
+// // // var calculatedNum=(2+2)*(4+10);
+// // // var calculatedNum=(2+(2*4))+10;
+// // // var calculatedNum=4/2*2;
+
+
+// // // var num ="2"+"2";
+// // // alert(num)
+
+chapter 16
+// // // message=("hello"+"Dolly"+"!");
+// // // alert(message);
+
+
+
+// // alert(333);
+
+
+// // var num=("Pakistan"+"Zinadabad");
+
+// // var num1 =("33"+33)
+// // alert(num1);
+
+// // alert("pakistan"+"zindabad");
+
+
+// // var  firstname=prompt("enter first name")
+
+// // var ans=prompt("enter your country" ,"china");
+
+chapter 17
+
+// // var yourName =prompt("enter your name");
+
+
+// // var ans=prompt("enter your country" ,"china");
+
+// // var a  ="what is your country";
+// // var b = "china";
+// // var c =prompt(a,b);
+
+chapter 18
+// //  var a  ="what is your country";
+// //  var b = "china";
+// //  var c =prompt(a,b);
+// //  alert(c);
+
+// // var city="karachi";
+// // if (city==="karachi"){
+// //     console.log("the city of lights");
+// // }
+// // x=5;
+// // y=5;
+
+
+// // if(x===y){
+// //    var z= prompt("enter tha value of z");
+
+// // }
+
+chapter 19
+// // var c = prompt("enter your zip code");
+// // var b ="10010"
+// // if(c==b){
+// //     alert("correct")
+// // }else{
+// //     alert("wrong")
+// // }
+
+// // var a =2;
+// // var b =3;
+// // if(a==2){
+// //     x=1;
+// //   console.log(x);
+// // }
+
+
+// // var a=2;
+// // var b=3;
+// // if (a !=b);
+
+// if(a>=b);
+
+
+// if (a!=5){
+//     a=10;
+//     console.log(a);
+// }
+
+
+// if(a !=b){
+//     alert("congratulations")
+// }
+
+
+chapter 20
+// var c =prompt("enter your name");
+// var d ="yousuf";
+// if(c!=d){
+//     alert("no match")
+// }
+
+// var a  = 20;
+// var b=10;
+
+
+// if(a>b){
+//     alert("yes found")
+// }else{
+//     a
+
+
+chapter 21
+
